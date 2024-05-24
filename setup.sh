e361ecd4d571ba275fc05c89ac9c344f1e2318e2
Shiphan <140245703+Shiphan@users.noreply.github.com>
2024-05-24 20:42:50 +0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
+cd $(dirname "$0")
+
 mkdir -p bin
 echo "Compiling..."
 g++ -o bin/snackdaemon main.cpp
-echo "Compiled."
 
 echo
-read -p "sudo copy compiled binary to /usr/bin? [y/N] " yn
+read -p "sudo copy compiled binary file to /usr/bin? [y/N] " yn
 case $yn in
     [Yy]* ) sudo cp ./bin/snackdaemon /usr/bin/snackdaemon;;
     * ) ;;
