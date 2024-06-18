4206c4553f24c76988bbbb2b845aa7e0c83b70c7
Shiphan <140245703+Shiphan@users.noreply.github.com>
2024-06-18 16:12:10 +0800

    update test of socket

--- tests/test12.cpp
+++ tests/test12.cpp
@@ -1,57 +1,82 @@
-#include <asm-generic/socket.h>
 #include <iostream>
 #include <cstring>
 #include <string>
 
 #include <unistd.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-
-#define PORT 8194
+#include <sys/un.h>
 
 void openDaemon() {
 	int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-	int int1 = 1;
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &int1, sizeof(int1));
+	unlink("/tmp/snackdaemon");
 
-	sockaddr_in addr;
-	addr.sin_family = AF_UNIX;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(PORT);
+	sockaddr_un addr;
+	strcpy(addr.sun_path, "/tmp/snackdaemon");
+	addr.sun_family = AF_UNIX;
 	if (bind(sockfd, (sockaddr *)&addr, sizeof(addr)) == -1) {
 		perror("bind error");
-		return;
 	}
 
-	listen(sockfd, 3);
+	if (listen(sockfd, 3) == -1) {
+		perror("listen error");
+	}
 
-	int clisock = accept(sockfd, nullptr, nullptr);
-	
-	char buffer[1024] = {};
-	recv(clisock, buffer, sizeof(buffer), 0);
-	std::cout << "message: " << buffer << std::endl;
+	while (true) {
+		int clisockfd = accept(sockfd, nullptr, nullptr);
+		
+		char buffer[1024] = {};
+		if (recv(clisockfd, buffer, sizeof(buffer), 0) == -1) {
+			perror("recv error");
+		}
+		if (strcmp(buffer, "ping") == 0) {
+			std::string mess("pong");
+			send(clisockfd, mess.c_str(), mess.length(), 0);
+			std::cout << "pinged!!" << std::endl;
 
+		} else {
+			std::cout << "message: " << buffer << std::endl;
+		}
+		close(clisockfd);
+	}
 	close(sockfd);
-	close(clisock);
+	unlink("/tmp/snackdaemon");
 }
 
 void sendMessage(std::string message) {
 	int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-	sockaddr_in addr;
-	addr.sin_family = AF_UNIX;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(PORT);
-
+	sockaddr_un addr;
+	strcpy(addr.sun_path, "/tmp/snackdaemon");
+	addr.sun_family = AF_UNIX;
 	if (connect(sockfd, (sockaddr *)&addr, sizeof(addr)) == -1) {
-		std::cout << "connect error, errno" << errno << std::endl;
-		return;
+		perror("connect error");
 	}
 	
 	send(sockfd, message.c_str(), message.length(), 0);
 	std::cout << "message: " << message << std::endl;
 	close(sockfd);
+}
+
+void pingDaemon() {
+	int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+	sockaddr_un addr;
+	strcpy(addr.sun_path, "/tmp/snackdaemon");
+	addr.sun_family = AF_UNIX;
+	if (connect(sockfd, (sockaddr *)&addr, sizeof(addr)) == -1) {
+		perror("connect error");
+	}
+	
+	std::string message("ping");
+	send(sockfd, message.c_str(), message.length(), 0);
+
+	char buffer[1024] = {};
+	recv(sockfd, buffer, sizeof(buffer), 0);
+	std::cout << buffer << std::endl;
+	close(sockfd);
+
+
 }
 
 int main(int argc, char* argv[]) {
@@ -61,6 +86,11 @@ int main(int argc, char* argv[]) {
 	} else if (argc == 2 && strcmp(argv[1], "send") == 0) {
 		std::cout << "send" << std::endl;
 		sendMessage("hello!!!");
+	} else if (argc == 2 && strcmp(argv[1], "ping") == 0) {
+		pingDaemon();
+	} else if (argc == 3 && strcmp(argv[1], "send") == 0) {
+		std::cout << "send" << std::endl;
+		sendMessage(argv[2]);
 	} else {
 		std::cout << "daemon or send plz" << std::endl;
 	}
